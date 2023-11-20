d7039dd0c06ea3c36ff7d5998eeebf40d1146139
Ariel Don <ariel@arieldon.com>
2023-11-19 19:15:52 -0600

    Define types `intptr` and `uintptr`

--- src/base.h
+++ src/base.h
@@ -8,19 +8,21 @@
 
 #include <sys/types.h>
 
-typedef  uint8_t    u8;
-typedef   int8_t    s8;
-typedef uint16_t   u16;
-typedef  int16_t   s16;
-typedef uint32_t   u32;
-typedef  int32_t   s32;
-typedef uint64_t   u64;
-typedef  int64_t   s64;
-typedef   size_t usize;
-typedef  ssize_t ssize;
-typedef      s32   b32;
-typedef    float   f32;
-typedef   double   f64;
+typedef uint8_t u8;
+typedef int8_t s8;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint32_t u32;
+typedef int32_t s32;
+typedef uint64_t u64;
+typedef int64_t s64;
+typedef size_t usize;
+typedef ssize_t ssize;
+typedef uintptr_t uintptr;
+typedef intptr_t sintptr;
+typedef s32 b32;
+typedef float f32;
+typedef double f64;
 
 typedef struct String String;
 struct String
